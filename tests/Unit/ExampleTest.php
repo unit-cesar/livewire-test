ee943ce7b9f0133a91447d340cd32dde67c1a013
unit-cesar <unit.cesar@gmail.com>
2025-04-08 12:55:26 -0300

    build: Checa ambiente antes dos testes

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,7 +3,8 @@
 namespace Tests\Unit;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use PHPUnit\Framework\TestCase;
+// use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
